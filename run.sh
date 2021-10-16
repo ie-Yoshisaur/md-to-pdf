24df27aebd7b449465a0454d11d75320a24044e3
e205723 <e205723@ie.u-ryukyu.ac.jp>
2021-10-17 02:26:46 +0900

    add another pandoc option :titlepage=true

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 name=$1
 docker-compose up -d
-docker exec -it md-to-pdf_app_1 pandoc -H tex/preamble.tex markdown/$name.md -f markdown -o pdf/$name.pdf -V documentclass=ltjarticle -V classoption=a4j -V geometry:margin=1in --listings --pdf-engine=lualatex --metadata=tables:true 
+docker exec -it md-to-pdf_app_1 pandoc -H tex/preamble.tex markdown/$name.md -f markdown -o pdf/$name.pdf -V documentclass=ltjarticle -V classoption=a4j -V geometry:margin=1in -V titlepage=true --listings --pdf-engine=lualatex --metadata=tables:true 
 docker container stop md-to-pdf_app_1 && docker container rm md-to-pdf_app_1
 
