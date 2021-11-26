dd73f1a80aaa9b3605f6abb2523ce536bf7f831c
e205723 <e205723@ie.u-ryukyu.ac.jp>
2021-11-27 05:11:34 +0900

    make docker-compose.yml compatible with new Docker-Desktop versions

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 name=$1
 docker-compose up -d
-docker exec -it md-to-pdf_app_1 pandoc -H tex/preamble.tex markdown/$name.md -f markdown -o pdf/$name.pdf -V documentclass=ltjarticle -V classoption=a4j -V geometry:margin=1in --listings --pdf-engine=lualatex --metadata=tables:true 
-docker container stop md-to-pdf_app_1 && docker container rm md-to-pdf_app_1
-
+docker exec -it md-to-pdf pandoc -H tex/preamble.tex markdown/$name.md -f markdown -o pdf/$name.pdf -V documentclass=ltjarticle -V classoption=a4j -V geometry:margin=1in --listings --pdf-engine=lualatex --metadata=tables:true 
+docker container stop md-to-pdf && docker container rm md-to-pdf
+open pdf/$name.pdf
