43a893019a536d4e371082ad53b03752a26ecc71
e205723 <e205723@ie.u-ryukyu.ac.jp>
2023-01-09 01:18:49 +0900

    fix: create pdf and latex files

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 mdfilename=$1
 filename=`echo ${mdfilename} | sed -e "s/^.*\///g" | sed -e "s/\.md$//g"`
 docker-compose up -d
-docker exec -it md-to-pdf pandoc -H tex/preamble.tex markdown/$filename.md -f markdown -o pdf/$filename.pdf -o latex/$filename.tex -V documentclass=ltjarticle -V classoption=a4j -V geometry:margin=1in --listings --pdf-engine=lualatex --metadata=tables:true
+docker exec -it md-to-pdf pandoc -H tex/preamble.tex markdown/$filename.md -f markdown -o latex/$filename.tex -V documentclass=ltjarticle -V classoption=a4j -V geometry:margin=1in --listings --pdf-engine=lualatex --metadata=tables:true
+docker exec -it md-to-pdf pandoc -H tex/preamble.tex markdown/$filename.md -f markdown -o pdf/$filename.pdf -V documentclass=ltjarticle -V classoption=a4j -V geometry:margin=1in --listings --pdf-engine=lualatex --metadata=tables:true
 docker container stop md-to-pdf && docker container rm md-to-pdf
 open pdf/$filename.pdf
